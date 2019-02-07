c119d38f4b313dcd6d788af4532c4bb6d16226c1
Supanat <supanat.ji@gmail.com>
2019-02-07 16:11:43 +0700

    pt2 done

--- Scratch/pt2/app.py
+++ Scratch/pt2/app.py
@@ -8,7 +8,14 @@ app = Flask(__name__)
 
 articles_data = Articles()
 
-
+# Config MySQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'Rx123456'
+app.config['MYSQL_DB'] = 'myflaskapp'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+# init MYSQL
+mysql = MySQL(app)
 
 @app.route('/')
 def index():
@@ -42,10 +49,31 @@ class RegisterForm(Form):
 @app.route('/register',methods=['GET','POST'])
 def method_name():
    form = RegisterForm(request.form)
-   if request.method == 'POST' and form.validators():
-      return render_template('register.html')
+   if request.method == 'POST' and form.validate():
+      name = form.name.data
+      email = form.email.data
+      username = form.username.data
+      password = sha256_crypt.encrypt(str(form.password.data))
+
+      # Create cursor
+      cur = mysql.connection.cursor()
+
+        # Execute query
+      cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", (name, email, username, password))
+
+      # Commit to DB
+      mysql.connection.commit()
+
+      # Close connection
+      cur.close()
+
+      flash('You are now registered and can log in', 'success')
+
+
+      return redirect(url_for('index'))
    return render_template('register.html',form=form)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+   app.secret_key='secret123'
+   app.run(debug=True)
